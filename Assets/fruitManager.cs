bff9fc613fc111bbc2910382a7714d02957d9e15
matuteale <mbuscagl@itba.edu.ar>
2018-08-26 21:59:13 -0300

    Add random fruit prefab, its manager and eating fruit logic in player

---
+++ Assets/fruitManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitManager : MonoBehaviour {
+
+	GameObject fruit;
+
+	// Use this for initialization
+	void Start () {
+		this.fruit = GameObject.Find("Fruit");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.newFruitIfNeeded();
+	}
+
+ 	void newFruitIfNeeded(){
+		if(!this.fruit.activeSelf){
+			this.fruit.transform.position = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f), 0);
+			this.fruit.SetActive(true);
+		}
+ }
+}
